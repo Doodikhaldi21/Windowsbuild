6b6d7bdb55f41681060ea83e3b92c263878c4555
Doodikhaldi21 <106251167+Doodikhaldi21@users.noreply.github.com>
2022-05-25 21:10:55 +0300

    Add files via upload

---
+++ Wavy shit.lua
@@ -0,0 +1,13 @@
+function onCreate()
+
+    makeLuaSprite('bg', 'bg', -500, -100);
+
+    setLuaSpriteScrollFactor('bg', 0.9, 0.9);
+
+    addGlitchEffect('bg', 2.2);
+
+    addLuaSprite('bg', false)
+
+close(true); 
+
+end
